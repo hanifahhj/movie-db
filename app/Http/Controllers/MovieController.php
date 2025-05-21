8c08cac38a91b20dbc3b9086f3a403977ede7e77
hanifahhj <hanifah.hj05@gmail.com>
2025-05-21 14:15:44 +0700

    store form movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,12 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
   public function index(){
-    $movies = Movie::latest()->take(9)->get(); // Ambil 9 film terbaru
-    return view('layouts.template', compact('movies'));
+    // $movies = Movie::latest() memanggil semua
+     $movies = Movie::latest()->paginate(6); // Ambil 6 film terbaru
+    return view('homepage', compact('movies'));
+   // return view('homepage, ['movie'-> $movies]);
   }
+
+  public function detail_movie($id, $slug){
+    $movie = Movie::find($id);
+    return view('movie_detail', compact('movie'));
+  }
+
+  public function create(){
+    $categories = Category::all();
+    return view('movie_form', compact('categories'));
+  }
+ public function store(Request $request)
+{
+    // Proses upload cover
+    $cover = $request->file('cover_image');
+    $namaFileCover = time() . '_' . $cover->getClientOriginalName();
+    $cover->move(public_path('covers'), $namaFileCover);
+
+    Movie::create([
+        'title' => $request->title,
+        'slug' => Str::slug($request->title),
+        'synopsis' => $request->synopsis,
+        'category_id' => $request->category_id,
+        'year' => $request->year,
+        'actors' => $request->actors,
+        'cover_image' => 'covers/' . $namaFileCover, // simpan folder + nama file
+    ]);
+
+    return redirect('/')->with('success', 'Movie berhasil ditambahkan!');
+}
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [MovieController::class,'index']);
+Route::get('/movie/{id}/{slug}', [MovieController::class,'detail_movie']);
+Route::get('/movie/create', [MovieController::class,'create']);
+Route::post('/movie/store', [MovieController::class,'store']);
